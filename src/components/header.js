ce1a541ccd52a7b3621c2e86d492b1173954a1ed
Jack Graymer <jackgraymer@gmail.com>
2023-03-02 19:42:34 +0100

    Adds basic style

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+function Header () {
+    return(
+        <div className="header">
+            <div>Header name</div>
+            <div>Timer</div>
+            <div>Links</div>
+        </div>
+    )
+}
+
+export default Header
