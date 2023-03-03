5bbe97e0622ccd8946bdcf15a1b424a97f2cdba0
Jack Graymer <jackgraymer@gmail.com>
2023-03-03 21:15:47 +0100

    Changes github logo in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom'
 import './header.css'
+import github from './square-github.svg'
+
+let startTime = Date.now()
 
 function Header () {
+
+    const [timer, setTimer] = useState(null)
+
+    /* useEffect(() => {
+        setInterval(function() {
+            let time = ((Date.now() - startTime)/1000).toFixed(2)
+            setTimer(time);
+        },100 );
+            
+    }) */
+
     return(
         <div className="header">
-            <div>Header name</div>
-            <div>Timer</div>
-            <div>Links</div>
+            <div className='title'><Link  to='/'>🅵🅸🅽🅳<span className='title-it'>🅸🆃</span></Link></div>
+            <div className='timer'>Timer {timer}</div>
+            <div className='links'>
+                <NavLink to='leaderboard'>Leaderboard</NavLink> 
+                <Link to='https://github.com/JackGraymer/FindIT-TOP-project' className='git-link' target={'_blank'}><img src={github} alt='Github Link'></img></Link>
+            </div>
         </div>
     )
 }
